06b87c8b892c08816ba494477812e61a545f20ce
Michael Berry <michael@saperemarketing.com>
2024-10-28 17:07:28 -0400

    Begin building racer

---
+++ app/Http/Controllers/RacerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreRacerRequest;
+use App\Http\Requests\UpdateRacerRequest;
+use App\Models\Racer;
+
+class RacerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRacerRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Racer $racer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Racer $racer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRacerRequest $request, Racer $racer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Racer $racer)
+    {
+        //
+    }
+}
