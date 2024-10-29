e5fcea81de1f9f7d94f3446e3ff1f68f4a2c0715
Michael <michael@saperemarketing.com>
2024-10-28 23:01:24 -0400

    Create car (#5)
    * Add car
    
    * Define car factory

---
+++ database/factories/CarFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Racer;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Car>
+ */
+class CarFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'racer_id' => Racer::factory(),
+        ];
+    }
+}
