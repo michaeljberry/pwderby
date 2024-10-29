c176258ac847e47b8a533521fa9a48c99204044b
Michael <michael@saperemarketing.com>
2024-10-28 22:17:49 -0400

    Begin building racer (#4)

---
+++ database/factories/RacerFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Racer>
+ */
+class RacerFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
+            'trophy_eligible' => fake()->boolean(),
+            'checked_in' => fake()->boolean()
+        ];
+    }
+}
