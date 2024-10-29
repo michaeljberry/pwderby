2e0bda97482baa0d50919991910f38f64fa546ff
Michael Berry <michael@saperemarketing.com>
2024-10-29 11:37:44 -0400

    Add round

---
+++ app/Http/Controllers/RoundController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreRoundRequest;
+use App\Http\Requests\UpdateRoundRequest;
+use App\Models\Round;
+
+class RoundController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreRoundRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Round $round)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Round $round)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateRoundRequest $request, Round $round)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Round $round)
+    {
+        //
+    }
+}
