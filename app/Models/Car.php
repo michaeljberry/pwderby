a1b77dafbf03608027d9c39874d5ec7465877b64
Michael Berry <michael@michaeljberry.com>
2024-10-28 23:18:04 -0400

    Define racer <-> car relationship

--- app/Models/Car.php
+++ app/Models/Car.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Car extends Model
 {
     use HasFactory;
+
+    public function racer(): BelongsTo
+    {
+        return $this->belongsTo(Racer::class);
+    }
 }
