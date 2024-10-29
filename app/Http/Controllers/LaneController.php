64a8f4e8b101c24acda406a08c5d7fa06c73bb70
Michael <michael@saperemarketing.com>
2024-10-29 11:26:39 -0400

    Add lanes (#7)

---
+++ app/Http/Controllers/LaneController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreLaneRequest;
+use App\Http\Requests\UpdateLaneRequest;
+use App\Models\Lane;
+
+class LaneController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLaneRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Lane $lane)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Lane $lane)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLaneRequest $request, Lane $lane)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Lane $lane)
+    {
+        //
+    }
+}
