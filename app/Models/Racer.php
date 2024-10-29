b4c7802af26116a73236b8ca3e7acb78d5fbfb15
Michael <michael@saperemarketing.com>
2024-10-28 23:18:48 -0400

    Define racer <-> car relationship (#6)

--- app/Models/Racer.php
+++ app/Models/Racer.php
@@ -2,10 +2,16 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Racer extends Model
 {
     use HasFactory;
+
+    public function cars(): HasMany
+    {
+        return $this->hasMany(Car::class)->chaperone();
+    }
 }
